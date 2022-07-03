8b0e9c965d47fb2ed130330a7a7db13d39419025
jaeskaaa <jaeskaaa@gmail.com>
2022-07-03 20:30:59 +0900

    (feat) actuator 이용한 무중단 config server refresh 테스트

---
+++ ingredient-client/src/main/java/com/client/ingredientclient/config/GreetingProps.java
@@ -0,0 +1,18 @@
+package com.client.ingredientclient.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "greeting")
+@Component
+public class GreetingProps {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
